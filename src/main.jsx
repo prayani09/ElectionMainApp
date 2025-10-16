ee4322492817882e1ef39f9649ffabc3789f7cf4
prayani <prayanichipade@gmail.com>
2025-10-16 16:07:49 +0530

    error slove

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { LanguageProvider } from './context/LanguageContext'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
